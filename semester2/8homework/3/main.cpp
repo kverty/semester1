c3caa31e97116a3147d5df6632f448fb1170f042
kverty <danila_yumsh@mail.ru>
2015-05-05 19:01:41 +0300

    instead of magic number used const

--- semester2/8homework/3/main.cpp
+++ semester2/8homework/3/main.cpp
@@ -1,10 +1,13 @@
 #include "xsandos.h"
 #include <QApplication>
 
+/// size of the field
+const int size = 5;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    XsAndOs w(5);
+    XsAndOs w(size);
     w.show();
 
     return a.exec();
