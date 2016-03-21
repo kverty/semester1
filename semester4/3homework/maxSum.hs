f70dc78bc948fb6739892be63cfe325a48eaf467
kverty <danila_yumsh@mail.ru>
2016-03-21 18:11:58 +0300

    убрал троичное вызывание одной и той же функции теперь сложность нормальная

--- semester4/3homework/maxSum.hs
+++ semester4/3homework/maxSum.hs
@@ -1,6 +1,8 @@
 maxSum :: [Int] -> Int
 maxSum [_] = -1
 maxSum [_,_] = 0 
-maxSum (x : list) = if (x + head list > (list !! maxSum list) + (list !! (1 + maxSum list)))
+maxSum (x : list) = 
+    let i = maxSum list
+    in if (x + head list > (list !! i) + (list !! (1 + i)))
                     then 0
-                    else (1 + maxSum list)
+                    else (1 + i)
